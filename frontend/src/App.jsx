f8c2d1281698d62c6e5d14bbd3b98bfba00613fc
Antoni Cabida <acabidasales@gmail.com>
2024-03-01 10:34:00 +0100

    Realizado el estilo principal de la página

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -9,20 +9,20 @@ function App() {
   return (
     <>
         {/* Navbar superior con logo y login */}
-        <nav id="navbar-hero" className="bg-zinc-900 bg-gradient-to-r from-sky-900 to-zync-900 flex w-screen">
-        <div className='flex-1 flex justify-center mr-auto max-w-48 mt-auto mb-auto'></div>
-          <div className="mx-12">
-              <a href='/'>
-                <img className="navbar-logo" src="/logo.png"/>
+        <nav id="navbar-hero" className="bg-zinc-900 bg-gradient-to-r from-sky-900 to-zync-900 w-screen h-[75px]">
+        <div className='relative inline-flex justify-center w-screen h-full items-center'>
+          <div className="">
+              <a href='/' className='w-full h-full inline-flex justify-center'>
+                <img className="aspect-[4.41/1] w-1/4 max-w-[1250px] min-w-[300px]" src="/logo-texto-lados.png"/>
               </a>
           </div>
-          
-          <div className='flex-1 flex justify-center ml-auto max-w-48 mt-auto mb-auto'>Login</div>
+          <div className='absolute flex justify-center mr-16 max-w-48 mt-auto mb-auto right-0'>Login</div>
+        </div>
         </nav>
 
         {/* Navbar */}
         <div className='bg-zinc-800 sticky top-0 w-full max-h-16'>
-            <ul className="nav navbar-nav flex">
+            <ul className="navbar-nav flex">
               <li className="nav-item active relative group flex" href='/'>
                 <a className="nav-link m-4" href='/'>
                   <div className='w-full h-full flex justify-center items-center'>
@@ -62,9 +62,11 @@ function App() {
         <div className='max-w-screen-md mx-auto'>
           <h2 className='text-xl font-bold mt-4 mb-4'>Inicio</h2>
           <div className='max-w-screen-md mx-auto bg-zinc-800'>
-            <div className=''>
-              
-              <img className="hero-up mx-auto" src="/hero-img.png"/>
+            <div className='relative max-w-screen-md'>
+              <img className='absolute w-1/3 top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2' src="/logo-texto-blanco.png"/>
+              <div className='bg-black bg-opacity-0'>
+                <img className="hero-up" src="/hero-img.png"/>
+              </div>
             </div>
             <div>
                 <div className='m-4 pb-4'>
