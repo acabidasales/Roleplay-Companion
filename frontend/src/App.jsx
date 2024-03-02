3d9434704e1821c104012a10232a31e07d806754
Antoni Cabida <acabidasales@gmail.com>
2024-03-02 15:49:16 +0100

    Añadido el router react

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,4 +1,5 @@
 import { useState } from 'react'
+import { Link, Outlet } from "react-router-dom";
 import './App.css'
 import './output.css'
 import 'boxicons'
@@ -23,8 +24,8 @@ function App() {
         {/* Navbar */}
         <div className='bg-zinc-800 sticky top-0 w-full max-h-16'>
             <ul className="navbar-nav flex">
-              <li className="nav-item active relative group flex" href='/'>
-                <a className="nav-link m-4" href='/'>
+              <Link to="/inicio" className="nav-item active relative group flex">
+                <a className="nav-link m-4">
                   <div className='w-full h-full flex justify-center items-center'>
                     <img className="max-w-10 inline-block max-h-4 mr-2" src='/home-icon.png'/>
                     <p className='inline-block align-middle text-lg group-hover:text-sky-500'>Inicio</p>
@@ -32,10 +33,10 @@ function App() {
                 </a>
                 <span className="absolute bottom-0 left-1/2 w-0 h-1 bg-sky-500 group-hover:w-1/2 group-hover:transition-all"></span>
                 <span className="absolute bottom-0 right-1/2 w-0 h-1 bg-sky-500 group-hover:w-1/2 group-hover:transition-all"></span>
-              </li>
+              </Link>
 
-              <li className="nav-item relative group flex" href='/'>
-                <a className="nav-link m-4" href='/'>
+              <li to="/inicio2" className="nav-item relative group flex">
+                <a className="nav-link m-4">
                   <div className='w-full h-full flex justify-center items-center'>
                     <box-icon name='food-menu' color='#ffffff' ></box-icon>
                     <p className='inline-block align-middle text-lg group-hover:text-sky-500 ml-1'>Mis personajes</p>
@@ -59,26 +60,7 @@ function App() {
         </div>
 
         {/*App content*/}
-        <div className='max-w-screen-md mx-auto'>
-          <h2 className='text-xl font-bold mt-4 mb-4'>Inicio</h2>
-          <div className='max-w-screen-md mx-auto bg-zinc-800'>
-            <div className='relative max-w-screen-md'>
-              <img className='absolute w-1/3 top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2' src="/logo-texto-blanco.png"/>
-              <div className='bg-black bg-opacity-0'>
-                <img className="hero-up" src="/hero-img.png"/>
-              </div>
-            </div>
-            <div>
-                <div className='m-4 pb-4'>
-                  <h2 className='text-lg font-semibold mt-4 mb-4'>Bienvenido a Roleplay Companion</h2>
-                  <p>
-                    Una web que te acompañará en tus aventuras y te dará un fácil acceso a tus personajes y campañas. Desde crear tus personajes hasta consultar recursos, todo al alncance de tu mano.<br></br><br></br>
-                    Actualmente solamente dispondrá de un juego de rol personalizado, utilizando bases de 
-                  </p>
-                </div>
-            </div>
-          </div>
-        </div>
+          <Outlet/>
 
         {/*Footer*/}
         <footer>
---
+++ frontend/src/pages/inicio.jsx
@@ -0,0 +1,33 @@
+import { useState } from 'react'
+import '../App.css'
+import '../output.css'
+import 'boxicons'
+
+function Inicio() {
+  return (
+    <>
+      <div className='max-w-screen-md mx-auto'>
+        <h2 className='text-xl font-bold mt-4 mb-4'>Inicio</h2>
+        <div className='max-w-screen-md mx-auto bg-zinc-800'>
+            <div className='relative max-w-screen-md'>
+                <img className='absolute w-1/3 top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2' src="/logo-texto-blanco.png"/>
+                <div className='bg-black bg-opacity-0'>
+                    <img className="hero-up" src="/hero-img.png"/>
+                </div>
+            </div>
+            <div>
+            <div className='m-4 pb-4'>
+                <h2 className='text-lg font-semibold mt-4 mb-4'>Bienvenido a Roleplay Companion</h2>
+                <p>
+                Una web que te acompañará en tus aventuras y te dará un fácil acceso a tus personajes y campañas. Desde crear tus personajes hasta consultar recursos, todo al alncance de tu mano.<br></br><br></br>
+                Actualmente solamente dispondrá de un juego de rol personalizado, utilizando bases de 
+                </p>
+            </div>
+         </div>
+        </div>
+      </div>
+    </>
+  )
+}
+
+export default Inicio
