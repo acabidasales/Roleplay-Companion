d50236236556a3e4b7556aca162700c13330dd79
Antoni Cabida <acabidasales@gmail.com>
2024-03-07 23:01:10 +0100

    Arreglando deploy

--- frontend/src/app/personajes/page.js
+++ frontend/src/app/personajes/page.js
@@ -44,7 +44,7 @@ export default function Personajes() {
             <h2 className='text-xl font-bold mt-4 mb-4 ml-4 pt-2 text-white'>Tus personajes</h2>
             <div className='grid grid-cols-3 gap-8'>
                 {data_personajes.map(data => 
-                    <a href={"/personajes/"+data.id} className="flex flex-col items-center bg-zinc-800 border border-zinc-900 rounded-lg shadow md:flex-row md:max-w-lg hover:bg-zinc-900">
+                    <a key={data.id} href={"/personajes/"+data.id} className="flex flex-col items-center bg-zinc-800 border border-zinc-900 rounded-lg shadow md:flex-row md:max-w-lg hover:bg-zinc-900">
                         <img className="object-cover w-full rounded-t-lg h-96 md:h-auto md:w-48 md:rounded-none md:rounded-s-lg" src={"/personajes/"+data.imagen} alt=""/>
                         <div className="flex flex-col justify-between p-4 leading-normal">
                             <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">{data.nombre}</h5>
