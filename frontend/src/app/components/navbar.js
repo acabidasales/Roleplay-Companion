8d02dba39442c97c17813c3211590ba32115b7aa
Antoni Cabida <acabidasales@gmail.com>
2024-03-09 17:09:49 +0100

    Añadidos colores en el navbar dependiendo de la ruta activa

--- frontend/src/app/components/navbar.js
+++ frontend/src/app/components/navbar.js
@@ -1,8 +1,19 @@
+"use client"
 import Link from "next/link";
 import "../globals.css";
 import 'boxicons/css/boxicons.min.css'
+import { usePathname } from 'next/navigation'
+import { useState, useEffect } from "react";
 
 export default function Navbar() {
+    const location = usePathname();
+    const [url, setUrl] = useState("");
+
+    useEffect(() => {
+        setUrl(location)
+    }, [location])
+
+
   return (
     <div className='bg-zinc-800 sticky -top-1 w-full max-h-16 z-50'>
         <ul className="navbar-nav flex">
@@ -10,33 +21,43 @@ export default function Navbar() {
             <div className="nav-link m-4">
             <div className='w-full h-full flex justify-center items-center'>
                 <img className="max-w-10 inline-block max-h-5 mr-2 " src='/home-icon.png'/>
-                <p className='inline-block align-middle text-lg group-hover:text-sky-500 text-white'>Inicio</p>
+                <p className={"inline-block align-middle text-lg group-hover:text-sky-500 " + (url === "/" ? "text-sky-500" : "text-white")}>Inicio</p>
             </div>
             </div>
-            <span className="absolute bottom-0 left-1/2 w-0 h-1 bg-sky-500 group-hover:w-1/2 group-hover:transition-all"></span>
-            <span className="absolute bottom-0 right-1/2 w-0 h-1 bg-sky-500 group-hover:w-1/2 group-hover:transition-all"></span>
+            <span className={"absolute bottom-0 left-1/2 w-0 h-1 bg-sky-500 " + (url === "/" ? "w-1/2 transition-all" : "group-hover:w-1/2 group-hover:transition-all")}></span>
+            <span className={"absolute bottom-0 right-1/2 w-0 h-1 bg-sky-500 " + (url === "/" ? "w-1/2 transition-all" : "group-hover:w-1/2 group-hover:transition-all")}></span>
         </Link>
 
         <Link id="1" href="/personajes" className="nav-item relative group flex">
             <div className="nav-link m-4">
             <div className='w-full h-full flex justify-center items-center'>
                 <i className='bx bx-food-menu bx-sm text-white'></i>
-                <p className='inline-block align-middle text-lg group-hover:text-sky-500 ml-1 text-white'>Mis personajes</p>
+                <p className={"inline-block align-middle text-lg group-hover:text-sky-500 " + (url === "/personajes" ? "text-sky-500" : "text-white")}>Mis personajes</p>
             </div>
             </div>
-            <span className="absolute bottom-0 left-1/2 w-0 h-1 bg-sky-500 group-hover:w-1/2 group-hover:transition-all"></span>
-            <span className="absolute bottom-0 right-1/2 w-0 h-1 bg-sky-500 group-hover:w-1/2 group-hover:transition-all"></span>
+            <span className={"absolute bottom-0 left-1/2 w-0 h-1 bg-sky-500 " + (url === "/personajes" ? "w-1/2 transition-all" : "group-hover:w-1/2 group-hover:transition-all")}></span>
+            <span className={"absolute bottom-0 right-1/2 w-0 h-1 bg-sky-500 " + (url === "/personajes" ? "w-1/2 transition-all" : "group-hover:w-1/2 group-hover:transition-all")}></span>
         </Link>
 
         <Link id="2" href="/campañas" className="nav-item relative group flex">
             <div className="nav-link m-4">
             <div className='w-full h-full flex justify-center items-center'>
                 <i className="bx bx-diamond bx-sm text-white"></i>
-                <p className='inline-block align-middle text-lg group-hover:text-sky-500 ml-1 text-white'>Campañas</p>
+                <p className={"inline-block align-middle text-lg group-hover:text-sky-500 " + (url === "/campañas" ? "text-sky-500" : "text-white")}>Campañas</p>
             </div>
             </div>
-            <span className="absolute bottom-0 left-1/2 w-0 h-1 bg-sky-500 group-hover:w-1/2 group-hover:transition-all"></span>
-            <span className="absolute bottom-0 right-1/2 w-0 h-1 bg-sky-500 group-hover:w-1/2 group-hover:transition-all"></span>
+            <span className={"absolute bottom-0 left-1/2 w-0 h-1 bg-sky-500 " + (url === "/campañas" ? "w-1/2 transition-all" : "group-hover:w-1/2 group-hover:transition-all")}></span>
+            <span className={"absolute bottom-0 right-1/2 w-0 h-1 bg-sky-500 " + (url === "/campañas" ? "w-1/2 transition-all" : "group-hover:w-1/2 group-hover:transition-all")}></span>
+        </Link>
+        <Link id="3" href="/wiki" className="nav-item relative group flex">
+            <div className="nav-link m-4">
+            <div className='w-full h-full flex justify-center items-center'>
+                <i className='bx bx-food-menu bx-sm text-white'></i>
+                <p className={"inline-block align-middle text-lg group-hover:text-sky-500 " + (url === "/personajes" ? "text-sky-500" : "text-white")}>Wiki</p>
+            </div>
+            </div>
+            <span className={"absolute bottom-0 left-1/2 w-0 h-1 bg-sky-500 " + (url === "/wiki" ? "w-1/2 transition-all" : "group-hover:w-1/2 group-hover:transition-all")}></span>
+            <span className={"absolute bottom-0 right-1/2 w-0 h-1 bg-sky-500 " + (url === "/wiki" ? "w-1/2 transition-all" : "group-hover:w-1/2 group-hover:transition-all")}></span>
         </Link>
         </ul>
     </div>
--- frontend/src/app/layout.js
+++ frontend/src/app/layout.js
@@ -11,7 +11,7 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="es">
-      <body className="bg-[url('/background-min-blur.png')] bg-fixed h-screen w-full bg-no-repeat bg-cover bg-left">
+      <body className="bg-[url('/background-min-blur.png')] bg-fixed h-screen w-full bg-no-repeat bg-cover bg-left text-white">
         {/* Navbar superior con logo y login */}
         <Top_navbar/>
 
